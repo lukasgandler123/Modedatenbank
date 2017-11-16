3c3165436282ddf7d4565042d83945af41516ad0
lukas.gandler <lukas.gandler@outlook.com>
2017-11-16 11:09:13 +0100

    benutzer bearbeiten

--- Lagerverwaltung - Mode/Lagerverwaltung - Mode/Benutzer_Bearbeiten.Designer.cs
+++ Lagerverwaltung - Mode/Lagerverwaltung - Mode/Benutzer_Bearbeiten.Designer.cs
@@ -28,11 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_Abbrechen = new System.Windows.Forms.Button();
+            this.tlp_Benutzer = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // btn_Abbrechen
+            // 
+            this.btn_Abbrechen.Location = new System.Drawing.Point(310, 558);
+            this.btn_Abbrechen.Name = "btn_Abbrechen";
+            this.btn_Abbrechen.Size = new System.Drawing.Size(215, 45);
+            this.btn_Abbrechen.TabIndex = 0;
+            this.btn_Abbrechen.Text = "Abbrechen";
+            this.btn_Abbrechen.UseVisualStyleBackColor = true;
+            this.btn_Abbrechen.Click += new System.EventHandler(this.btn_Abbrechen_Click);
+            // 
+            // tlp_Benutzer
+            // 
+            this.tlp_Benutzer.AutoScroll = true;
+            this.tlp_Benutzer.AutoSize = true;
+            this.tlp_Benutzer.ColumnCount = 4;
+            this.tlp_Benutzer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 57.05882F));
+            this.tlp_Benutzer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 42.94118F));
+            this.tlp_Benutzer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 175F));
+            this.tlp_Benutzer.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 339F));
+            this.tlp_Benutzer.Location = new System.Drawing.Point(12, 12);
+            this.tlp_Benutzer.Name = "tlp_Benutzer";
+            this.tlp_Benutzer.RowCount = 1;
+            this.tlp_Benutzer.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlp_Benutzer.Size = new System.Drawing.Size(514, 0);
+            this.tlp_Benutzer.TabIndex = 1;
+            // 
+            // Benutzer_Bearbeiten
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(964, 615);
+            this.Controls.Add(this.tlp_Benutzer);
+            this.Controls.Add(this.btn_Abbrechen);
+            this.Name = "Benutzer_Bearbeiten";
             this.Text = "Benutzer_Bearbeiten";
+            this.Load += new System.EventHandler(this.Benutzer_Bearbeiten_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Abbrechen;
+        private System.Windows.Forms.TableLayoutPanel tlp_Benutzer;
     }
 }
--- Lagerverwaltung - Mode/Lagerverwaltung - Mode/Benutzer_Bearbeiten.cs
+++ Lagerverwaltung - Mode/Lagerverwaltung - Mode/Benutzer_Bearbeiten.cs
@@ -7,14 +7,80 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Lagerverwaltung___Mode
 {
     public partial class Benutzer_Bearbeiten : Form
     {
+        MySqlConnection con = new MySqlConnection("server = eduweb.kb.local; user id = team07; password = T3amO7; database = team07");
+
         public Benutzer_Bearbeiten()
         {
             InitializeComponent();
+        }
+
+        private void btn_Abbrechen_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Benutzer_Bearbeiten_Load(object sender, EventArgs e)
+        {
+            MySqlCommand select = new MySqlCommand("SELECT * FROM t_benutzer;", con);
+
+            // Liste mit allen Benutzern
+            List<Benutzer> benutzerListe = new List<Benutzer>();
+
+            try
+            {
+                con.Open();
+                MySqlDataReader reader = select.ExecuteReader();
+                while (reader.Read())
+                {
+                    //reihen++;
+                    // Liste befüllen
+                    Benutzer bn = new Benutzer(reader[1].ToString(), reader[2].ToString(), reader[3].ToString(), reader[4].ToString(), reader[5].ToString());
+                    benutzerListe.Add(bn);
+                }
+                con.Close();
+            }
+            catch(MySqlException ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+            //MessageBox.Show("reihen: " + reihen.ToString());
+            //MessageBox.Show(benutzerListe.Count.ToString());
+
+            tlp_Benutzer.RowCount = benutzerListe.Count;
+            TabelleBefuellen(benutzerListe, benutzerListe.Count);
+        }
+
+        private void TabelleBefuellen(List<Benutzer> liste, int reihen)
+        {
+            tlp_Benutzer.Controls.Add(new Label() { Text = "Hi1" }, 0, 0);
+            tlp_Benutzer.Controls.Add(new Label() { Text = "Hi2" }, 0, 1);
+            tlp_Benutzer.Controls.Add(new Label() { Text = "Hi3" }, 0, 2);
+        }
+    }
+
+    // Benutzer, die gespeichert werden
+    public class Benutzer
+    {
+        public string vorname { get; set; }
+        public string nachname { get; set; }
+        public string email { get; set; }
+        public string rolle { get; set; }
+        public string benutzername { get; set; }
+
+        public Benutzer(string vorname, string nachname, string email, string rolle, string benutzername)
+        {
+            this.vorname = vorname;
+            this.nachname = nachname;
+            this.email = email;
+            this.rolle = rolle;
+            this.benutzername = benutzername;
         }
     }
 }
